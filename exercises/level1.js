5bcac47f83c01cbd7247862a52dea5daab555dc4
Stephen McNearney <stephen@hownow.media>
2021-01-11 09:57:25 -0500

    Initial Commit - DigitalCrafts Week 4 Day 5 - JavaScript 101 Exercises - Weekend Homework

---
+++ exercises/level1.js
@@ -0,0 +1,42 @@
+function lvl1exercise1 () {
+  // Declare a variable without instantiating it and return it!
+  const basicVariable = "variable";
+    return basicVariable;
+}
+
+function lvl1exercise2 () {
+  // Declare and instantiate a number and return it
+  const number = 3;
+    return number;
+}
+
+function lvl1exercise3 () {
+  // Declare and instantiate a floating point number that is not a whole number and return it
+  const floatingPointNumber = 1.5;
+    return floatingPointNumber;
+}
+
+function lvl1exercise4 () {
+  // Declare and instantiate a string "Hello World!" and return it
+  const helloWorld = "Hello World!";
+    return helloWorld;
+
+}
+
+function lvl1exercise5 () {
+  // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+  const newArray = ["Hello World!", 4];
+    return newArray;
+}
+
+function lvl1exercise6 () {
+  // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+  const keysValues = { key1 : "Hello World!", key2 : 4 };
+    return keysValues;
+}
+
+function lvl1exercise7 () {
+  // Declare and instantiate a boolean value 'false' and return it
+  const aLie = false;
+    return aLie;
+}
---
+++ exercises/level6.js
@@ -0,0 +1,32 @@
+function lvl6exercise1 (num) {
+  // Return 'hello' if num is 1, 'world' if num is 2, otherwise return 'bye'
+  const myReply;
+  if (num = 1) {
+    myReply = 'hello';
+  } 
+  else if (num = 2) {
+    myReply = 'world';
+  } 
+  else {
+    myReply = 'bye';
+  }
+
+  return myReply;
+
+}
+
+function lvl6exercise2 () {
+  // Push 10 'hello' strings into the array using a for loop, then return it
+  var arr = []
+  for arr.length {
+    
+    arr.push("hello")
+  }
+  return arr;
+  
+}
+
+function lvl6exercise3 () {
+  // Empty this array using a while loop and return it
+  var arr = ['hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello', 'hello']
+}
